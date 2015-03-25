6e95c1513bb94f5a1f9b69e474564771199926cc
Tõnis Simo <anton.estum@gmail.com>
2015-03-25 07:21:34 +0300

    Avoid crashing when ActionMailer::Base not defined

--- app/mailers/clearance_mailer.rb
+++ app/mailers/clearance_mailer.rb
@@ -1,14 +1,16 @@
-class ClearanceMailer < ActionMailer::Base
-  def change_password(user)
-    @user = user
-    mail(
-      from: Clearance.configuration.mailer_sender,
-      to: @user.email,
-      subject: I18n.t(
-        :change_password,
-        scope: [:clearance, :models, :clearance_mailer],
-        default: "Change your password"
+if defined? ActionMailer::Base
+  class ClearanceMailer < ActionMailer::Base
+    def change_password(user)
+      @user = user
+      mail(
+        from: Clearance.configuration.mailer_sender,
+        to: @user.email,
+        subject: I18n.t(
+          :change_password,
+          scope: [:clearance, :models, :clearance_mailer],
+          default: "Change your password"
+        )
       )
-    )
+    end
   end
 end
